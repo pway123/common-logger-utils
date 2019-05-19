40c027eeca6e1a55a1df25f1b03ed6de7c0a1086
John Lee <>
2019-05-20 00:06:40 +0800

    [PG-601] [John ] [Common Logger Utils Library] - Refactor logger js to avoid repeated check

--- logger.js
+++ logger.js
@@ -1,80 +1,73 @@
 const loggerUtils = require('./loggerUtils');
+const TYPE = {
+    INFO: 'info',
+    DEBUG: 'debug',
+    ERROR: 'error',
+    WARN: 'warn'
+}
 
 class logger {
-    isObject(data) {
-        return typeof data !== 'object' ? {} : data;
-    }
-
-    isString(data) {
-        return typeof data !== 'string' ? undefined : data;
-    }
-
-    isDate(date) {
-        return Object.prototype.toString.call(date) === '[object Date]' ? date : undefined;
-    }
-
     error(trxType, trxResult, trxId, startTime, others, err) {
-        let loggerObj = {
-            trxType: this.isString(trxType),
-            trxResult: this.isString(trxResult),
-            trxId: this.isString(trxId),
-            startTime: this.isDate(startTime),
-            others: Object.keys(this.isObject(others)).length > 0 ? {
-                ...others,
-                metadata: {
-                    ...others.metadata,
-                    internalErrorInfo: {
-                        errorReason: err.stack || err.message
-                    }
-                }
-            } : {}
-        }
-
-        loggerUtils.error('', loggerObj);
+        processLogFormat(TYPE.ERROR, trxType, trxResult, trxId, startTime, others, err);
     }
 
     warn(trxType, trxResult, trxId, startTime, others, err) {
-        let loggerObj = {
-            trxType: this.isString(trxType),
-            trxResult: this.isString(trxResult),
-            trxId: this.isString(trxId),
-            startTime: this.isDate(startTime),
-            others: Object.keys(this.isObject(others)).length > 0 ? {
-                ...others,
-                metadata: {
-                    ...others.metadata,
-                    internalErrorInfo: {
-                        errorReason: err.stack || err.message
-                    }
-                }
-            } : {}
-        }
-
-        loggerUtils.warn('', loggerObj);
+        processLogFormat(TYPE.ERROR, trxType, trxResult, trxId, startTime, others, err)
     }
 
     info(trxType, trxResult, trxId, startTime, others) {
-        let loggerObj = {
-            trxType: this.isString(trxType),
-            trxResult: this.isString(trxResult),
-            trxId: this.isString(trxId),
-            startTime: this.isDate(startTime),
-            others: this.isObject(others)
-        }
-
-        loggerUtils.info('', loggerObj);
+        processLogFormat(TYPE.INFO, trxType, trxResult, trxId, startTime, others);
     }
 
     debug(trxType, trxResult, trxId, startTime, others) {
-        let loggerObj = {
-            trxType: this.isString(trxType),
-            trxResult: this.isString(trxResult),
-            trxId: this.isString(trxId),
-            startTime: this.isDate(startTime),
-            others: this.isObject(others)
-        }
+        processLogFormat(TYPE.DEBUG, trxType, trxResult, trxId, startTime, others);
+    }
+}
 
+function isObject(data) {
+    return typeof data !== 'object' ? {} : data;
+}
+
+function isString(data) {
+    return typeof data !== 'string' ? undefined : data;
+}
+
+function isDate(date) {
+    return Object.prototype.toString.call(date) === '[object Date]' ? date : undefined;
+}
+
+function processLogFormat(type, trxType, trxResult, trxId, startTime, others, err) {
+    let loggerObj = {
+        trxType: isString(trxType),
+        trxResult: isString(trxResult),
+        trxId: isString(trxId),
+        startTime: isDate(startTime),
+        others: Object.keys(isObject(others)).length > 0 ? {
+            ...others,
+            metadata: {
+                ...others.metadata,
+                // internalErrorInfo: {
+                //     errorReason: err.stack || err.message
+                // }
+            }
+        } : {}
+    }
+
+    if ((type === TYPE.ERROR || type === TYPE.WARN) && loggerObj.others.metadata) {
+        loggerObj.others.metadata.internalErrorInfo = { errorReason: err.stack || err.message }
+    }
+
+    if (type === TYPE.INFO) {
+        loggerUtils.info('', loggerObj);
+    }
+    else if (type === TYPE.DEBUG) {
         loggerUtils.debug('', loggerObj);
+    }
+    else if (type === TYPE.WARN) {
+        loggerUtils.warn('', loggerObj);
+    }
+    else {
+        loggerUtils.error('', loggerObj);
     }
 }
 
